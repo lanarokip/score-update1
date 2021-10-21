cb6d41cfe64d09b24556322af02e056f79d58178
lanarokip <lankipkoecharo@gmail.com>
2021-10-21 11:41:47 +0300

    added the butterKnife binding inthe onCereate section

--- app/src/main/java/com/kipkoech/petfinderip/PetActivity.java
+++ app/src/main/java/com/kipkoech/petfinderip/PetActivity.java
@@ -3,12 +3,28 @@ package com.kipkoech.petfinderip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PetActivity extends AppCompatActivity {
+    private static final String TAG = PetActivity.class.getSimpleName();
+    @BindView(R.id.errorTextView)
+    TextView mErrorTextView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView)
+    ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet);
+        ButterKnife.bind(this);
     }
 }
