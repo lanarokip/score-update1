919df3151b44b33120829407c002508a07cb945e
lanarokip <lankipkoecharo@gmail.com>
2021-10-21 11:36:04 +0300

    created anempty activity where ill beable to display the data fetched from the API

---
+++ app/src/main/java/com/kipkoech/petfinderip/petActivity.java
@@ -0,0 +1,14 @@
+package com.kipkoech.petfinderip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class petActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet);
+    }
+}
