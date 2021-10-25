b537581835101d67e4c4028ad5ced34c96c3ed9e
lanarokip <lankipkoecharo@gmail.com>
2021-10-25 09:31:42 +0300

    addedthe constant class that will hold the url link together with the API key link

---
+++ app/src/main/java/com/kipkoech/petfinderip/Constants.java
@@ -0,0 +1,9 @@
+package com.kipkoech.petfinderip;
+
+public class Constants {
+    public static final String BASE_URL= "https://api.petfinder.com/v2/";
+    public static final String access_token = BuildConfig.API_KEY;
+    public static final String PREFERENCES_LOCATION_KEY = "gender";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "gender";
+}
+
