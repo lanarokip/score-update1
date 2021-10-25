83233188653399c8ebc402a1ab88a41752d683b8
lanarokip <lankipkoecharo@gmail.com>
2021-10-25 09:32:49 +0300

    added the user authentication that is the login page enabling users to login to the app

---
+++ app/src/main/java/com/kipkoech/petfinderip/ui/user_registration.java
@@ -0,0 +1,16 @@
+package com.kipkoech.petfinderip.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kipkoech.petfinderip.R;
+
+public class user_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+    }
+}
